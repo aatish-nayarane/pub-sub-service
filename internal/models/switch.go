2a88937e628a22cdbe1ae8e7d37f443cc60e2e52
aatish narayane <aatish.n@netwebindia.com>
2026-06-30 00:22:10 +0530

    use  switch  mode l for  json  and bson  data

--- internal/models/switch.go
+++ internal/models/switch.go
@@ -1,7 +1,8 @@
 package models
 type Switch struct {
-	Project_id string `json:"project_id"`
-	User_id    string `json:"user_id"`
-	Email_enable bool `json:"email_enable"`
-	Audit_enable bool `json:"audit_enable"`
+	Project_id string `json:"project_id" bson:"project_id"`
+	User_id    string `json:"user_id" bson:"user_id"`
+	Email_enable bool `json:"email_enable" bson:"email_enable"`
+	Audit_enable bool `json:"audit_enable" bson:"audit_enable"`
 }
+
